1532ead1e0e4a4593f870197662d8390f0f4a2b5
PiraTechnics <devin1younge@gmail.com>
2023-11-11 13:24:21 -0800

    astro-decap-cms add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,35 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import NetlifyCMS from "astro-decap-cms";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: "https://example.com",
+	integrations: [
+		mdx(),
+		sitemap(),
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: "git-gateway",
+					branch: "main",
+				},
+				collections: [
+					// Define a blog post collection
+					{
+						name: "posts",
+						label: "Blog Posts",
+						folder: "src/content/blog",
+						create: true,
+						delete: true,
+						fields: [
+							{ name: "title", widget: "string", label: "Post Title" },
+							{ name: "body", widget: "markdown", label: "Post Body" },
+						],
+					},
+				],
+			},
+		}),
+	],
 });
