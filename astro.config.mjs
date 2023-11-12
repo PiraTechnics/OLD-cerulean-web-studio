d4b02200379485fd6a9aead12d53fa9f9359b1c3
PiraTechnics <devin1younge@gmail.com>
2023-11-11 19:32:42 -0800

    TailwindCSS added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,40 +3,47 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import NetlifyCMS from "astro-decap-cms";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [
-		NetlifyCMS({
-			config: {
-				backend: {
-					name: "git-gateway",
-					branch: "main",
-				},
-				collections: [
-					// Define a blog post collection
-					{
-						name: "posts",
-						label: "Blog Posts",
-						folder: "src/content/blog",
-						create: true,
-						delete: true,
-						fields: [
-							{ name: "title", widget: "string", label: "Post Title" },
-							{
-								name: "description",
-								widget: "string",
-								label: "Post Description",
-							},
-							{ name: "pubDate", widget: "datetime", label: "Publish Date" },
-							{ name: "heroImage", widget: "image", label: "Featured Image" },
-							{ name: "body", widget: "markdown", label: "Post Body" },
-						],
-					},
-				],
-			},
-		}),
-		mdx(),
-		sitemap(),
-	],
+  site: "https://example.com",
+  integrations: [NetlifyCMS({
+    config: {
+      backend: {
+        name: "git-gateway",
+        branch: "main"
+      },
+      collections: [
+      // Define a blog post collection
+      {
+        name: "posts",
+        label: "Blog Posts",
+        folder: "src/content/blog",
+        create: true,
+        delete: true,
+        fields: [{
+          name: "title",
+          widget: "string",
+          label: "Post Title"
+        }, {
+          name: "description",
+          widget: "string",
+          label: "Post Description"
+        }, {
+          name: "pubDate",
+          widget: "datetime",
+          label: "Publish Date"
+        }, {
+          name: "heroImage",
+          widget: "image",
+          label: "Featured Image"
+        }, {
+          name: "body",
+          widget: "markdown",
+          label: "Post Body"
+        }]
+      }]
+    }
+  }), mdx(), sitemap(), tailwind()]
 });
