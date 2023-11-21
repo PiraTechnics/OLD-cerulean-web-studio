1160659bfebe9a381e3cbe4925680276a04976dc
PiraTechnics <devin1younge@gmail.com>
2023-11-20 21:49:54 -0800

    Dropdown nav animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,26 +2,7 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {
-			/* animation: {
-				slidedown: "slidedown 1s",
-			},
-			keyframes: {
-				slidedown: {
-					"0%": { transform: "translateY(-500px)", opacity: "0" },
-					"100%": { transform: "none", opacity: "1" },
-				},
-			},
-			animation: {
-				slideup: "slideup 1s",
-			},
-			keyframes: {
-				slidedown: {
-					"0%": { transform: "none", opacity: "1" },
-					"100%": { transform: "translateY(-500px)", opacity: "0" },
-				},
-			}, */
-		},
+		extend: {},
 	},
 	plugins: [require("daisyui")],
 	daisyui: {
