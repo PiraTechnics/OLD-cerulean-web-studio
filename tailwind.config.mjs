7b1e1fbed522f7ec931a162dcea42f29c08b171d
PiraTechnics <devin1younge@gmail.com>
2023-11-20 18:46:32 -0800

    Page Template and Dropdown Nav

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			/* animation: {
+				slidedown: "slidedown 1s",
+			},
+			keyframes: {
+				slidedown: {
+					"0%": { transform: "translateY(-500px)", opacity: "0" },
+					"100%": { transform: "none", opacity: "1" },
+				},
+			},
+			animation: {
+				slideup: "slideup 1s",
+			},
+			keyframes: {
+				slidedown: {
+					"0%": { transform: "none", opacity: "1" },
+					"100%": { transform: "translateY(-500px)", opacity: "0" },
+				},
+			}, */
+		},
 	},
 	plugins: [require("daisyui")],
 	daisyui: {
